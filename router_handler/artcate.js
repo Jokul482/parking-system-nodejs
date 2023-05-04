cb15c856803dd5dbf1ccbcae31b58e6765242918
谭鑫森 <849610435@qq.com>
2023-05-04 14:03:55 +0800

    文章列表增删改查

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -51,3 +51,59 @@ exports.addArticleCates = (req, res) => {
         });
     });
 };
+
+// 删除文章分类的处理函数
+exports.deleteCateById = (req, res) => {
+    // 定义标记删除的 SQL 语句
+    const sql = 'update ev_article_cate set is_delete=1 where id=?'
+    // 调用 db.query() 执行 SQL 语句
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) return res.cc(err);
+        if (results.affectedRows !== 1) return res.cc('删除文章分类失败！');
+        res.cc('删除文章分类成功！', 0)
+    })
+}
+
+// 根据id获取文章分类的处理函数
+exports.getArtCateById = (req, res) => {
+    // 定义根据id获取文章分类的 SQL 语句 
+    const sql = 'select * from ev_article_cate where id=?'
+    db.query(sql, req.params.id, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err);
+        // SQL 语句执行成功，但是没有查询到任何参数
+        if (results.length !== 1) return res.cc('获取文章分类数据失败！')
+        // 把数据响应给客户端
+        res.send({
+            status: 0,
+            message: '获取文章分类成功！',
+            data: results[0]
+        })
+    })
+}
+
+// 根据 Id 更新文章分类的处理函数
+exports.updateCateById = (req, res) => {
+    // 定义查重的 SQL 语句
+    const sql = `select * from ev_article_cate where Id<>? and (name=? or alias=?)`
+    // 调用 db.query() 执行查重的 SQL 语句
+    db.query(sql, [req.body.Id, req.body.name, req.body.alias], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+
+        // 判断名称和别名被占用的4种情况
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+
+        // 定义更新文章分类的 SQL 语句
+        const sql = `update ev_article_cate set ? where Id=?`
+        // 执行更新文章分类的 SQL 语句
+        db.query(sql, [req.body, req.body.Id], (err, results) => {
+            if (err) return res.cc(err)
+            if (results.affectedRows !== 1) return res.cc('更新文章分类失败！')
+            res.cc('更新文章分类成功！', 0)
+        })
+    })
+}
--- schema/artcate.js
+++ schema/artcate.js
@@ -4,10 +4,33 @@ const joi = require('joi')
 // 2、定义 name 和 alias 的验证规则
 const name = joi.string().required()
 const alias = joi.string().alphanum().required()
+// id的校验规则
+const id = joi.number().integer().min(1).required()
 
 // 3、向外共享验证规则对象
 exports.add_cate_schema = {
     body: {
+        name,
+        alias
+    }
+}
+// 验证规则对象 - 删除分类
+exports.delete_cate_schema = {
+    params: {
+        id
+    }
+}
+// 验证规则对象 - 根据 id 获取列表分类
+exports.get_cate_schema = {
+    params: {
+        id
+    }
+}
+
+// 验证规则对象 - 根据 id 更新列表分类
+exports.update_cate_schema = {
+    body: {
+        Id:id,
         name,
         alias
     }
