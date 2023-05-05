f5e9f8a2677c2f7adcf01960694f909059aa935e
T-x-s <849610435@qq.com>
2023-05-06 00:36:11 +0800

    更新用户信息

--- router/userinfo.js
+++ router/userinfo.js
@@ -19,7 +19,7 @@ router.get('/getUserList', userinfo_handler.getUserList)
 // 获取用户的基本信息
 router.get('/getUserInfo', userinfo_handler.getUserInfo)
 // 更新用户的基本信息
-router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
+router.post('/updateUserInfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
 // 重置密码的路由
 router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 // 更新用户头像的路由
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -37,15 +37,13 @@ exports.getUserList = (req, res) => {
 exports.getUserInfo = (req, res) => {
     // 根据用户的 id，查询用户的基本信息
     // 注意：为了防止用户的密码泄露，需要排除 password 字段
-    const sql = `select id, role_type, username, nickname, email, user_pic from ev_users where id=?`;
+    const sql = `select id, role_type, username, nickname, email from ev_users where id=?`;
     // 注意：req 对象上的 user 属性，是 Token 解析成功，express-jwt 中间件帮我们挂载上去的
-    db.query(sql, req.user.id, (err, results) => {
+    db.query(sql, req.query.id, (err, results) => {
         // 1. 执行 SQL 语句失败
         if (err) return res.cc(err);
-
         // 2. 执行 SQL 语句成功，但是查询到的数据条数不等于 1
         if (results.length !== 1) return res.cc("获取用户信息失败！");
-
         // 3. 将用户信息响应给客户端
         res.send({
             status: 0,
@@ -57,7 +55,9 @@ exports.getUserInfo = (req, res) => {
 // 更新用户基本信息的处理函数
 exports.updateUserInfo = (req, res) => {
     const sql = `update ev_users set ? where id=?`;
-    db.query(sql, [req.body, req.body.id], (err, results) => {
+    // 对新密码进行 bcrypt 加密处理
+    const newPwd = bcrypt.hashSync(req.body.password, 10);
+    db.query(sql, [{...req.body, password: newPwd}, req.body.id], (err, results) => {
         // 执行 SQL 语句失败
         if (err) return res.cc(err);
 
