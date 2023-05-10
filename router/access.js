9f05db3e805f6e8fa12bcb9a00ff069ca4124708
谭鑫森 <849610435@qq.com>
2023-05-10 16:46:16 +0800

    车辆列表

--- router/access.js
+++ router/access.js
@@ -7,10 +7,12 @@ const registerHandler = require('../router_handler/access')
 // 1. 导入验证表单数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
-const { vehicle_schema } = require('../schema/access')
+// const {  } = require('../schema/access')
 // 获取车辆列表数据的路由
 router.get("/getRegistrationList", registerHandler.getVehicleRegistrationList)
+// 获取车位数据的路由
+router.get("/getVehicleInfo", registerHandler.getVehicleInfo)
 // 新增车辆登记数据的路由
-router.post("/AddVehicle", expressJoi(vehicle_schema), registerHandler.postAddVehicle)
+router.post("/postAddVehicle", registerHandler.postAddVehicle)
 
 module.exports = router
